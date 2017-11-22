1eca353c7e2f21c9338e6448c0bd3a18aed7b898
datascience2017 <rini.cba@gmail.com>
2017-11-22 15:35:04 -0500

    Add files via upload

---
+++ Week4Assignment/app.R
@@ -0,0 +1,69 @@
+library(shiny)
+
+# Define UI for application that draws a histogram
+ui <- 
+shinyUI(fluidPage(
+  # Application title
+  titlePanel("Predict Expense based on Year"),
+  sidebarLayout(
+    sidebarPanel(
+      sliderInput("sliderYear", "What is the Year?", value= 1950, min=1940,max=1960,step = 5),
+      checkboxInput("showPlots","Show/Hide Additional Plots",value=TRUE),
+      submitButton("Submit")
+    ),
+    mainPanel(
+      plotOutput("plot1", height = 350, width = 700) ,
+      h4("Predicted Expense from Model",height = 10, width = 10),
+      textOutput("pred1"),
+      conditionalPanel("input.showPlots==true", plotOutput("plot2",height=300),
+                       plotOutput("plot3",height=300))
+      
+    )
+  )
+)
+)
+
+library(shiny)
+library(tidyr)
+library(dplyr)
+
+# In this study, we are using the dataset: US Personal Expenditure from the year 1940 till 1960.
+# This study shows interactive plots using Shiny package.
+# I have also created a model using linear regression to predict Expenses based on year.
+# If you move the slider, to change the year(example : 1940 or 1950 etc), you will see predicted expenses for that year.
+# Plot 1: This plot displays the Expenses based on the year and also the predicted expenses as per the model created.
+# If click on the checkbox 'Show/hide Plot': you will be able to see additional plots that may be useful information about the data.
+# Plot 2: This boxplot displays the overall Expenditure across the years.
+# Plot 3: This boxplot displays the overall Expenditure across the expense categories(Medical, Food etc)
+
+datasets::USPersonalExpenditure
+Exp <- as.data.frame(USPersonalExpenditure)
+#Using tidyr to transform data from wide to long format
+d <- Exp %>% mutate(Category=as.factor(rownames(Exp))) %>% gather(year,Category) 
+colnames(d)= c("Category", "Year", "Expense")
+
+server <- shinyServer(function(input, output) {
+  
+  model1 <- lm(Expense~as.factor(Year), data = d)
+  model1pred <- reactive({predict(model1,newdata=data.frame(Year=input$sliderYear))})
+  
+  output$plot1 <- renderPlot({
+    plot(d$Year, d$Expense,xlab="Year", ylab="Expenses in billions", bty="n",pch=16,
+         xlim=c(1940,1960),ylim=c(0,80))
+    legend(25,250,c("Model 1 Prediction", "Model 2 Prediction"),pch=16,
+           col=c("red","blue"),bty="n",cex=1.2)
+    points(input$sliderYear,model1pred(),col="red", pch=16, cex=2)
+  })
+  output$plot2 <- renderPlot({
+    boxplot(d$Expense~d$Year,data=d,col=d$Category, main=" Summary of Expenses by Year",
+            xlab="Year", ylab="Expense in billions (USD)")
+  })
+  output$plot3 <- renderPlot({
+    plot(d$Category, d$Expense, col=d$Year, mode="markers", main="Summary of Expenses over the Years by Category",
+         xlab="Expense Category", ylab="Expense in billions (USD)")
+  })
+  output$pred1 <- renderText({model1pred()})
+})
+# Run the application 
+shinyApp(ui = ui, server = server)
+
